ec201b416216421b2018e92eaf58a2a96e8d78ea
Robiu1 <muhammadrobiu08@gmail.com>
2023-07-13 00:25:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - First Experience with C
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
